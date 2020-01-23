a1eda3231bb8229e569053c9be0c35ce294a5c68
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-01-23 21:03:19 +0100

    First tries for binding engine.

--- tradeables.py
+++ tradeables.py
@@ -1,29 +1,43 @@
 import json
+import os
+from datetime import datetime
 from pathlib import Path
 import pandas
-
+import sqlalchemy
+from sqlalchemy import create_engine
 import requests
+from sqlalchemy.orm import sessionmaker, Session
 
-user_id = 795
-#tradeables = requests.get(f"https://barter.vg/u/{user_id}/t/json/").json()
-#Path("tradeables.json").write_text(json.dumps(tradeables, indent=4), encoding='utf-8')
-tradeables = json.loads(Path("tradeables.json").read_text())
+from models import Fetches, Games, Statistics
 
-for game_id, game in tradeables['by_platform']['1'].items():
-    # print(game_id, game)
-    title = game['title']
-    tradeable = game.get("tradable", "n/a")
-    wishlist = game.get('wishlist', "n/a")
-    library = game.get('library', "n/a")
-    print(f"""
-Title: {title}
-Tradeable: {tradeable}
-Wishlists: {wishlist}
-Library: {library}""")
 
-df = pandas.DataFrame.from_dict(tradeables['by_platform']['1'], orient='index',
-                                columns=['title', 'tradable', 'wishlist', 'library', 'item_type'])
+def get_postgres_engine():
+    engine_type = 'postgresql'
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+    database = os.getenv("DATABASE")
+    server_url = os.getenv("SERVER_URL")
+    conn_string = f"{engine_type}://{username}:{password}@{server_url}/{database}"
+    return create_engine(conn_string)
 
-only_games = df[df['item_type'] == 'Game']
-only_games = only_games.drop(columns=['item_type'])
-print(only_games)
+
+class Rolling:
+    def __init__(self, session):
+        self.session = session
+        self.fetch = None
+
+    def create_fetch_instance(self):
+        self.fetch = Fetches(FETCH_DATE=datetime.now())
+        self.session.add(self.fetch)
+        self.session.commit()
+
+    def add_game(self):
+        game = Games(GAME_ID=1, GAME_TITLE="A", PRICE=999, RELEASE_YEAR=2010, REVIEWS_POSITIVE=80, REVIEWS_TOTAL=2222,
+                     ACHIEVEMENTS=100, CARDS=1)
+        statistics = Statistics(FETCH_ID=self.fetch, GAME_ID=game, TRADEABLES=100, WISHLIST=100, LIBRARY=100)
+
+
+engine = get_postgres_engine()
+session = Session(bind=engine)
+roll = Rolling(session)
+roll.create_fetch_instance()
