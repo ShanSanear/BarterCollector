d1046055458620873f1c31c575fb1692f3a1c04e
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-01-23 13:26:26 +0100

    Corrected misstype.

--- tradeables.py
+++ tradeables.py
@@ -5,14 +5,14 @@ import pandas
 import requests
 
 user_id = 795
-tradeables = requests.get(f"https://barter.vg/u/{user_id}/t/json/").json()
-Path("tradeables.json").write_text(json.dumps(tradeables, indent=4), encoding='utf-8')
-tradeables = json.load(Path("tradeables.json").open())
+#tradeables = requests.get(f"https://barter.vg/u/{user_id}/t/json/").json()
+#Path("tradeables.json").write_text(json.dumps(tradeables, indent=4), encoding='utf-8')
+tradeables = json.loads(Path("tradeables.json").read_text())
 
 for game_id, game in tradeables['by_platform']['1'].items():
     # print(game_id, game)
     title = game['title']
-    tradeable = game.get("tradeable", "n/a")
+    tradeable = game.get("tradable", "n/a")
     wishlist = game.get('wishlist', "n/a")
     library = game.get('library', "n/a")
     print(f"""
@@ -22,8 +22,7 @@ Wishlists: {wishlist}
 Library: {library}""")
 
 df = pandas.DataFrame.from_dict(tradeables['by_platform']['1'], orient='index',
-                                columns=['title', 'tradeable', 'wishlist', 'library', 'bundles_available',
-                                         'bundles_all', 'item_type'])
+                                columns=['title', 'tradable', 'wishlist', 'library', 'item_type'])
 
 only_games = df[df['item_type'] == 'Game']
 only_games = only_games.drop(columns=['item_type'])
