0df0b7d86daae1d8a5bf59e698b26132befc9e96
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-01-23 21:02:23 +0100

    Added table names and changed schema to "barter".

--- models.py
+++ models.py
@@ -1,15 +1,18 @@
-from sqlalchemy import String, Integer, Column, TIMESTAMP, ForeignKey
+from sqlalchemy import String, Integer, Column, TIMESTAMP, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+Base = declarative_base(metadata=MetaData(schema='barter'))
 
 
 class Fetches(Base):
+    __tablename__ = 'FETCHES'
     FETCH_ID = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
     FETCH_DATE = Column(TIMESTAMP, nullable=False)
 
 
 class Games(Base):
+    __tablename__ = 'GAMES'
     GAME_ID = Column(Integer, primary_key=True, nullable=False, unique=True)
     GAME_TITLE = Column(String, nullable=False)
     PRICE = Column(Integer)
@@ -21,6 +24,7 @@ class Games(Base):
 
 
 class Statistics(Base):
+    __tablename__ = "STATISTICS"
     STAT_ID = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
     FETCH_ID = Column(Integer, ForeignKey(Fetches.FETCH_ID))
     GAME_ID = Column(Integer, ForeignKey(Games.GAME_ID))
