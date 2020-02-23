a0a11fe9173bc2359a4b5d171d3938d0a9817cab
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-02-23 19:19:46 +0100

    Now adds games to rendering of the template.

--- presenter.py
+++ presenter.py
@@ -1,11 +1,36 @@
+from datetime import datetime, timedelta
+
 from flask import Flask, render_template
+
+from tradeables import create_dataframes_from_tradables
 
 app = Flask(__name__)
 
 
+class Fetcher:
+    def __init__(self):
+        self._cached_tradables_data = []
+        self._last_call = datetime.now()
+        self._max_diff = timedelta(hours=6)
+        self.fetch_tradables()
+
+    def fetch_tradables(self):
+        self._cached_tradables_data = create_dataframes_from_tradables('raw_data')
+        self._last_call = datetime.now()
+
+    @property
+    def dataframes(self):
+        if datetime.now() - self._last_call > self._max_diff:
+            self.fetch_tradables()
+        return self._cached_tradables_data
+
+
+fetcher = Fetcher()
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    games = fetcher.dataframes
+    return render_template('index.html', games=games)
 
 
 if __name__ == '__main__':
